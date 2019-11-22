2e3488aadcfb836fd7b0e266701e1c5a1be67709
RylanFerrer <rylanferrer@gmail.com>
2019-11-22 15:34:34 -0800

    added charts

---
+++ src/data/medianviews.js
@@ -0,0 +1,25 @@
+export default {
+    labels: [
+        'Art & Design',
+        'Technology',
+        'Science',
+        'Society',
+        'Health',
+        'Business'
+    ],
+    datasets: [
+        {
+            label: 'Median Number Of Views',
+            backgroundColor: '#333333',
+            borderColor: '#333333',
+            data: [
+                { x: 'Art', y: 1072306.0 },
+                { x: 'Tech', y: 979990.0},
+                { x: 'Science', y: 1043622.0 }, 
+                { x: 'Society', y: 1059114.0 },
+                { x: 'Health', y: 1183204.0 },
+                { x: 'Business', y: 1120643.0 },
+            ]
+        }
+    ]
+}

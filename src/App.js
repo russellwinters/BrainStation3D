bbc87852a5eb1b05dfdf7e70dbb934de0d5f334c
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-11-22 15:53:08 -0800

    added a pop up form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import PopularTopics from "./components/PopularTopics";
+import CreateNew from "./components/CreateNew";
 
 function App() {
   return (
     <>
       <Header />
       <PopularTopics />
+      <CreateNew />
     </>
   );
 }

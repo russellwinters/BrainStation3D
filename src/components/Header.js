3e3bb8bfa1e8cd7b58b5b158841547a36dc12cf7
russellwinters <russellwinters@protonmail.com>
2019-11-22 16:00:24 -0800

    Header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Logo from "../assets/white-bg-logo.svg";
 
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="header-title">Hello BrainStation</h1>
+      <img src={Logo} alt="TED Logo" />
+      <nav className="header-nav">
+        <span className="header-nav__link">Watch</span>
+        <span className="header-nav__link">Discover</span>
+        <span className="header-nav__link">Participate</span>
+        <span className="header-nav__link">About</span>
+      </nav>
     </header>
   );
 }
